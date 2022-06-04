a319cc3d02f22880bb65ddcb11bba0e85e7b3ec1
Guilherme <guilherme_milk_10@hotmail.com>
2022-06-04 15:21:52 -0400

    clouds and collision

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add("jump");
@@ -10,5 +11,28 @@ const jump = () => {
     },500);
 
 }
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log(marioPosition);
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+        pipe.style.animation = 'none';
+        //pipe.style.left = '${pipePosition}px'; isnt working
+        
+        mario.style.animation = 'none';
+        //mario.style.bottom = '${marioPosition}px'; isnt working
+    
+        mario.src = "/img/game-over.png";
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }
+
+},10)
 
 document.addEventListener('keydown', jump);
